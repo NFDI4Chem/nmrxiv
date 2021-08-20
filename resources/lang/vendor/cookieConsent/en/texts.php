e3e9641d1144177d36018b9822cf0d3e4068f43b
Venkata chandrasekhar Nainala <mailcs76@gmail.com>
2021-08-20 15:29:52 +0100

    Updated cookie content - english

--- resources/lang/vendor/cookieConsent/en/texts.php
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'Your experience on this site will be improved by allowing cookies.',
-    'agree' => 'Allow cookies',
+    'message' => 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our Privacy Notice and Terms of Use.',
+    'agree' => 'I agree, dismiss this banner',
 ];
