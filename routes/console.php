06355519e67c54aaba0ad45946aa3d33aa5cbbfa
Venkata chandrasekhar Nainala <vcnainala@gmail.com>
2021-08-20 13:23:19 +0100

    Initialised laravel, jetstream

---
+++ routes/console.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+Artisan::command('nmrxiv', function () {
+    $this->comment("Welcome to nmrXiv v0.1 Alpha");
+})->purpose('Display nmrxiv info');
