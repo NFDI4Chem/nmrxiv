4c124807b699de078fa9b9d7053b9a8ed7bbf204
Venkata chandrasekhar Nainala <mailcs76@gmail.com>
2021-08-20 16:58:53 +0100

    cookie consent visual changes

--- resources/lang/vendor/cookieConsent/en/texts.php
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'message' => 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our Privacy Notice and Terms of Use.',
+    'message' => 'This website requires cookies, and the limited processing of your personal data in order to function. By using the site you are agreeing to this as outlined in our <a class="text-blue-400" href="/privacy-policy"><u>Privacy Notice</u></a> and <a href="/terms"><u>Terms of Use</u></a>.',
     'agree' => 'I agree, dismiss this banner',
 ];
