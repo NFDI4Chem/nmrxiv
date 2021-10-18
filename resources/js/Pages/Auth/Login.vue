e6f731f36e64547169b5fd258553ec4f4a4a15fc
Nisha Sharma <sharmanis14@gmail.com>
2021-10-18 15:53:05 +0200

    conditional rendering single sign on

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -49,46 +49,10 @@
                     Not registered? Register here
                 </Link>
             </div>
+
+            <single-sign-on />
         </form>
-        <div class="mt-6" v-show="isSingleSignOn">
-        <div class="relative">
-          <div class="absolute inset-0 flex items-center">
-            <div class="w-full border-t border-gray-300"></div>
-          </div>
-          <div class="relative flex justify-center text-sm">
-            <span class="px-2 bg-white text-gray-500">
-              Or continue with
-            </span>
-          </div>
-        </div>
 
-        <div class="mt-6 grid grid-cols-3 gap-3">
-          <div v-show="isOrchid">
-            <a href="/auth/login/orcid" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
-              <span class="sr-only">Sign in with ORCID</span>
-              <svg style="height: 20px" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>ORCID icon</title><path d="M12 0C5.372 0 0 5.372 0 12s5.372 12 12 12 12-5.372 12-12S18.628 0 12 0zM7.369 4.378c.525 0 .947.431.947.947s-.422.947-.947.947a.95.95 0 0 1-.947-.947c0-.525.422-.947.947-.947zm-.722 3.038h1.444v10.041H6.647V7.416zm3.562 0h3.9c3.712 0 5.344 2.653 5.344 5.025 0 2.578-2.016 5.025-5.325 5.025h-3.919V7.416zm1.444 1.303v7.444h2.297c3.272 0 4.022-2.484 4.022-3.722 0-2.016-1.284-3.722-4.097-3.722h-2.222z"/></svg>
-            </a>
-          </div>
-
-          <div v-show="isTwitter">
-            <a href="/auth/login/twitter" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-900 hover:bg-gray-50">
-              <span class="sr-only">Sign in with Twitter</span>
-              <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-               <svg style="height: 20px" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title><path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" /></svg>
-              </svg>
-            </a>
-          </div>
-
-          <div v-show="isGithub">
-            <a href="/auth/login/github" class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-900 hover:bg-gray-50">
-              <span class="sr-only">Sign in with GitHub</span>
-              <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd" />
-              </svg>
-            </a>
-          </div>
-        </div>
-      </div>
     </jet-authentication-card>
 </template>
 
@@ -100,8 +64,8 @@
     import JetCheckbox from '@/Jetstream/Checkbox.vue'
     import JetLabel from '@/Jetstream/Label.vue'
     import JetValidationErrors from '@/Jetstream/ValidationErrors.vue'
-    import { Head, Link } from '@inertiajs/inertia-vue3';
-import { onBeforeMount } from '@vue/runtime-core'
+    import SingleSignOn from '@/App/SingleSignOn.vue';
+    import { Head, Link } from '@inertiajs/inertia-vue3'
 
     export default {
         components: {
@@ -113,9 +77,11 @@ import { onBeforeMount } from '@vue/runtime-core'
             JetCheckbox,
             JetLabel,
             JetValidationErrors,
+            SingleSignOn,
             Link,
         },
 
+      
         props: {
             canResetPassword: Boolean,
             status: String,
@@ -123,10 +89,6 @@ import { onBeforeMount } from '@vue/runtime-core'
 
         data() {
             return {
-                isSingleSignOn: false,
-                isOrchid: false,
-                isTwitter: false,
-                isGithub: false,
                 form: this.$inertia.form({
                     email: '',
                     password: '',
@@ -145,17 +107,6 @@ import { onBeforeMount } from '@vue/runtime-core'
                         onFinish: () => this.form.reset('password'),
                     })
             }
-        },
-
-        beforeMount(){ 
-            this.isOrchid = process.env.ORCID_CLIENT_ID ? true : false;
-            this.isTwitter = process.env.TWITTER_CLIENT_ID ? true : false;
-            this.isGithub = process.env.GITHUB_CLIENT_ID ? true : false;
-
-            if(this.isOrchid || this.isTwitter || this.isGithub){
-              this.isSingleSignOn = true;
-            }
-
         }
     }
 </script>
