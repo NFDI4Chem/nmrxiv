d51939d29b1343740fd564ee99fcf1ad03cfe633
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-07-01 10:47:38 +0200

    Working vite build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
+import '../css/app.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { InertiaProgress } from '@inertiajs/progress';
 import helpers from "./Mixins/Global.js";
 import Children from "@/Shared/Children.vue";
@@ -9,12 +11,13 @@ import InstantSearch from 'vue-instantsearch/vue3/es';
 import Vue3Tour from 'vue3-tour';
 import mitt from 'mitt';
 
+
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 const emitter = mitt();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => require(`./Pages/${name}.vue`),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,17 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'nmrXiv') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        @vite('resources/css/app.css')
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Scripts -->
         @routes
-        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased h-full">
         @inertia
