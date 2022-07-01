13e3683a1c10f4e55a902561d944a3552451f335
Shift <shift@laravelshift.com>
2022-07-01 07:37:15 +0000

    Use new `@vite` Blade directive

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,11 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        @vite('resources/css/app.css')
 
         <!-- Scripts -->
         @routes
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased h-full">
         @inertia
