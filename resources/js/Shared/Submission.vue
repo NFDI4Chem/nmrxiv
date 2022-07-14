40b16f49349dab321b9da14fce6cd21f0497aad5
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-07-14 13:43:29 +0200

    Migrated datepicker to the new repository

--- resources/js/Shared/Submission.vue
+++ resources/js/Shared/Submission.vue
@@ -1631,8 +1631,8 @@ import { Dropzone } from "dropzone";
 import axiosRetry from "axios-retry";
 import slider from "vue3-slider";
 import OCL from "openchemlib/full";
-import Datepicker from "vue3-date-time-picker";
-import "vue3-date-time-picker/dist/main.css";
+import Datepicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
 import SelectRich from "@/Shared/SelectRich.vue";
 import JetInputError from "@/Jetstream/InputError.vue";
 
