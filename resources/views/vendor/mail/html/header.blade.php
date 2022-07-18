f5cea849245480a614f68ee1efb93a33a4dcb6b2
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-07-18 10:50:59 +0200

    Pint code santisation Added logo to email header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-{{ $slot }}
+<img src="{{ asset('img/logo.svg') }}" alt="Logo">
 @endif
 </a>
 </td>
