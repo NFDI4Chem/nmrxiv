7dca0ea4bac7373cc31f7a648aca71157e24358a
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-07-20 13:07:48 +0200

    Fixing empty name issue

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,7 +33,7 @@ class CreateNewUser implements CreatesNewUsers
 
         return DB::transaction(function () use ($input) {
             return tap(User::create([
-                'name' => $input['first_name'] . ' ' . $input['last_name'],
+                'name' => $input['first_name'].' '.$input['last_name'],
                 'first_name' => $input['first_name'],
                 'last_name' => $input['last_name'],
                 'email' => $input['email'],
