418f17cd9de4c4e93cf5e0a9513d998993a96df0
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-08-09 00:51:07 +0200

    Fixes #247

--- resources/js/Pages/Study/About.vue
+++ resources/js/Pages/Study/About.vue
@@ -371,21 +371,6 @@
                                                     <div
                                                         class="text-center my-10 py-10"
                                                     >
-                                                        <svg
-                                                            class="mx-auto h-12 w-12 text-gray-400"
-                                                            fill="none"
-                                                            viewBox="0 0 24 24"
-                                                            stroke="currentColor"
-                                                            aria-hidden="true"
-                                                        >
-                                                            <path
-                                                                vector-effect="non-scaling-stroke"
-                                                                stroke-linecap="round"
-                                                                stroke-linejoin="round"
-                                                                stroke-width="2"
-                                                                d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-                                                            />
-                                                        </svg>
                                                         <h3
                                                             class="mt-2 text-sm font-medium text-gray-900"
                                                         >
