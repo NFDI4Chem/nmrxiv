49dd8519abe6857ba1e4ef95d2b684b58264ed7c
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-08-09 00:23:08 +0200

    Implemented file/folder delete in the submission flow

--- app/Events/DraftProcessed.php
+++ app/Events/DraftProcessed.php
@@ -2,13 +2,9 @@
 
 namespace App\Events;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
 class DraftProcessed implements ShouldBroadcastNow
 {
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,12 +2,12 @@
 
 namespace App\Providers;
 
+use App\Events\DraftProcessed;
+use App\Listeners\SendDraftProcessedNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
-use App\Events\DraftProcessed;
-use App\Listeners\SendDraftProcessedNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
