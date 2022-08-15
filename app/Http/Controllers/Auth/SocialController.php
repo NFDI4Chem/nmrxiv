4f6aa46e077de8da9f4fc2c02c158923004620d1
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-08-15 21:32:58 +0200

    Added name filed to the auth social controller register

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -57,6 +57,7 @@ class SocialController extends Controller
             }
             if (! $user) {
                 $user = tap(User::create([
+                    'name' => $providerUser->getName(),
                     'first_name' => explode(' ', $providerUser->getName(), 2)[0],
                     'last_name' => explode(' ', $providerUser->getName(), 2)[1],
                     'email' => $providerUser->getEmail(),
