296237d1637890a9c7a52d497f4ec4d887f11db4
NishaSharma14 <sharmanis14@gmail.com>
2022-08-22 16:29:48 +0200

    License WIP.

--- resources/js/Pages/Public/Project/License.vue
+++ resources/js/Pages/Public/Project/License.vue
@@ -16,7 +16,7 @@
                             <p
                                 style="max-width: 100ch !important"
                                 class="prose mt-1 text-sm text-blue-gray-500"
-                                v-html="project.data.license.description"
+                                 v-html="project.data.license.body"
                             ></p>
                         </div>
                     </div>
