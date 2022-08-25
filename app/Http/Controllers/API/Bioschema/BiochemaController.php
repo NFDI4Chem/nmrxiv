6229a6786bfe4a5aa1ca2877a3bf37c7be8cbc27
NRayya <82588017+NRayya@users.noreply.github.com>
2022-08-25 21:15:34 +0200

    Add dataset schema Modify project schema Modify study schema

--- app/Http/Controllers/API/Bioschema/BiochemaController.php
+++ app/Http/Controllers/API/Bioschema/BiochemaController.php
@@ -8,6 +8,7 @@ use Spatie\SchemaOrg\Schema;
 use App\Models\Bioschema\BioSchema;
 use App\Models\Project;
 use App\Models\Study;
+use App\Models\Dataset;
 use App\Models\Bioschema\Study as StudySchema;
 use App\Models\User;
 use App\Models\Author;
@@ -22,12 +23,12 @@ class BiochemaController extends Controller
         if($user){
             $project = Project::where([['slug', $projectName], ['owner_id', $user->id]])->firstOrFail();  
         }
-
         if($project){
             $creativeWork = Schema::creativeWork();
             $creativeWork['@id'] = 'https://bioschemas.org/profiles/Study/0.2-DRAFT';
             $confromsTo = [];
             $confromsTo['http://purl.org/dc/terms/conformsTo'] = $creativeWork;
+            
 
             $projectSchema = Schema::Project(); 
             $projectSchema['@id']= $project->uuid;
@@ -54,18 +55,35 @@ class BiochemaController extends Controller
             }
             $projectSchema->keywords($tags);
             $projectSchema->url($project->public_url);
+            $projectSchema->license($project->license->url);
             
-
-
-            if($studyName && !$datasetName){
-                $study = Study::where([['slug', $studyName], ['owner_id', $user->id], ['project_id', $project->id]])->firstOrFail();
+            $studies = [];
+            foreach ($project->studies as &$study) {
+                $studySchema = BioSchema::Study(); 
+                $studySchema->name($study->name);
+                $studySchema->url($study->public_url);
+                
+                $datasets = [];
+                foreach ($study->datasets as &$dataset) {
+                    $datasetSchema = Schema::Dataset(); 
+                    $datasetSchema->name($dataset->name);
+                    $datasetSchema->url($dataset->public_url);
+                    array_push($datasets, $datasetSchema);
+                }
+                $studySchema->hasPart($datasets);
+                array_push($studies, $studySchema);   
+            }
+            $projectSchema->hasPart($studies);
+            
+            if($studyName){
                 // send study back with project info added to it\
+                $study = Study::where([['slug', $studyName], ['owner_id', $user->id], ['project_id', $project->id]])->firstOrFail();
                 if($study){
                     $studySchema = BioSchema::Study(); 
                     $studySchema['@id']= $study->uuid;
                     $studySchema['dct:conformsTo'] = $confromsTo;
-                    $studySchema->description($study->description);
                     $studySchema->name($study->name);
+                    $studySchema->description($study->description);
                     $tags = [];
                     foreach ($study->tags as &$tag) {
                         $tag = $tag->name; 
@@ -73,30 +91,46 @@ class BiochemaController extends Controller
                     }
                     $studySchema->keywords($tags);
                     $studySchema->url($study->public_url);
-
+                    $studySchema->license($study->license->url);
+                    $datasets = [];
+                    foreach ($study->datasets as &$dataset) {
+                        $datasetSchema = Schema::Dataset(); 
+                        $datasetSchema->name($dataset->name);
+                        $datasetSchema->url($dataset->public_url);
+                        array_push($datasets, $datasetSchema);
+                    }
+                    $studySchema->hasPart($datasets);
                     $projectSchema->relatedStudy($studySchema);
+                    
+                    
+                    if($datasetName){
+                        $dataset = Dataset::where([['slug', $datasetName], ['owner_id', $user->id], ['project_id', $project->id], ['study_id', $study->id]])->firstOrFail();
+                        // send dataset with project and study details
+                        if($dataset){
+                            $dataCatalog = Schema::dataCatalog();
+                            $dataCatalog->name(env('APP_NAME'));
+                            $dataCatalog->url('https://nmrxiv.org');
 
-                    return $projectSchema;
+                            $creativeWorkDataset = Schema::creativeWork();
+                            $creativeWorkDataset['@id'] = 'https://bioschemas.org/profiles/Dataset/1.0-RELEASE';
+                            $DatasetconfromsTo = [];
+                            $DatasetconfromsTo['http://purl.org/dc/terms/conformsTo'] = $creativeWorkDataset;
+                            $datasetSchema = Schema::Dataset(); 
+                            $datasetSchema['@id']= $dataset->uuid;
+                            $datasetSchema['dct:conformsTo'] = $DatasetconfromsTo;
+                            $datasetSchema->description($dataset->description);
+                            $datasetSchema->name($dataset->name);
+                            $datasetSchema->url($dataset->public_url);
+                            $datasetSchema->license($study->license->url);
+                            $datasetSchema->includedInDataCatalog($dataCatalog);
+                            $datasetSchema->measurementTechnique("https://terminology.nfdi4chem.de/ts/ontologies/chmo/terms?iri=http%3A%2F%2Fpurl.obolibrary.org%2Fobo%2FCHMO_0000591");
+                            $datasetSchema->isPartOf([$projectSchema]);
+                            return $datasetSchema;
+                        }
+                    }
                 }
-                
-            }else if($studyName && $datasetName){
-                $dataset = Dataset::where([['slug', $datasetName], ['owner_id', $user->id], ['project_id', $project->id], ['study_id', $study->id]])->firstOrFail();
-                // send dataset with project and study details
-                return [];
-            }else{
-                // add studies and associated datasets
-                $studies = [];
-                foreach ($project->studies as &$study) {
-                    $studySchema = BioSchema::Study(); 
-                    $studySchema['@id']= $study->uuid;
-                    $studySchema['dct:conformsTo'] = $confromsTo;
-                    $studySchema->name($study->name);
-                    $studySchema->url($study->public_url);
-                    array_push($studies, $studySchema);   
-                }
-                $projectSchema->relatedStudy($studies);
-                return $projectSchema;
             }
+            return $projectSchema;
         }
     }
 }
