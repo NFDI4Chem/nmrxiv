184597c2a5e332c0495334ea73ee37d75cf5ce7d
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-09-28 11:15:24 +0200

    fix: Updated daily scheduled command for publishing models

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('telescope:prune')->daily();
-        $schedule->command('publish:projects')->daily();
+        $schedule->command('nmrxiv:publish')->daily();
     }
 
     /**
