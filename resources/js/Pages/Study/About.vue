87eaa26fccd4d2c8ca097f2e7c3455e95676a57b
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-10-13 14:04:00 +0200

    fix: Fixes - public studies - possible deletion of structures #464

--- resources/js/Pages/Study/About.vue
+++ resources/js/Pages/Study/About.vue
@@ -359,6 +359,7 @@
                                                                             </div>
                                                                         </div>
                                                                         <button
+                                                                            v-if="canUpdateStudy"
                                                                             class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                                                             @click="
                                                                                 deleteMolecule(
@@ -372,6 +373,7 @@
                                                                             >Delete
                                                                         </button>
                                                                         <button
+                                                                            v-if="canUpdateStudy"
                                                                             class="inline-flex ml-2 items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                                                             @click="
                                                                                 editMolecule(
