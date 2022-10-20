a3fc19313f5aa3de1f81295db18f3c91d832a279
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2022-10-20 11:12:07 +0200

    fix: Added/Updated commands publish, unpublish, assign dois, config based doi assignments, validation - schema version updates and code formatting

--- app/Models/HasDOI.php
+++ app/Models/HasDOI.php
@@ -6,77 +6,81 @@ trait HasDOI
 {
     public function generateDOI($doiService)
     {
-        $identifier = $this->getIdentifier($this, 'identifier');
+        $doi_host = env('DOI_HOST', null);
 
-        if ($this->doi == null) {
-            $authors = [];
-            $users = [];
-            $suffix = null;
-            $url = 'https://www.nmrxiv.org/';
-            if ($this instanceof Project) {
-                $users = $this->allUsers();
-                $authors = $this->authors ? $this->authors : [];
-                $suffix = 'P'.$identifier;
-                $url = $url.'P'.$identifier;
-            } elseif ($this instanceof Study) {
-                $users = $this->allUsers();
-                $authors = $this->project->authors ? $this->project->authors : [];
-                $projectIdentifier = $this->getIdentifier($this->project, 'identifier');
-                $suffix = 'P'.$projectIdentifier.'.'.'S'.$identifier;
-                $url = $url.'S'.$identifier;
-            } elseif ($this instanceof Dataset) {
-                $users = $this->study->allUsers();
-                $authors = $this->project->authors ? $this->project->authors : [];
-                $projectIdentifier = $this->getIdentifier($this->project, 'identifier');
-                $studyIdentifier = $this->getIdentifier($this->study, 'identifier');
-                $suffix = 'P'.$projectIdentifier.'.'.'S'.$studyIdentifier.'.'.'D'.$identifier;
-                $url = $url.'D'.$identifier;
-            }
+        if (! is_null($doi_host)) {
+            $identifier = $this->getIdentifier($this, 'identifier');
 
-            $creators = [];
-            foreach ($users as $user) {
-                array_push($creators, [
-                    'name' => $user->last_name.' '.$user->first_name,
-                    'nameType' => 'Personal',
-                    'givenName' => $user->first_name,
-                    'familyName' => $user->last_name,
-                    'affiliation' => [],
-                    'nameIdentifiers' => [],
-                ]);
-            }
+            if ($this->doi == null) {
+                $authors = [];
+                $users = [];
+                $suffix = null;
+                $url = 'https://www.nmrxiv.org/';
+                if ($this instanceof Project) {
+                    $users = $this->allUsers();
+                    $authors = $this->authors ? $this->authors : [];
+                    $suffix = 'P'.$identifier;
+                    $url = $url.'P'.$identifier;
+                } elseif ($this instanceof Study) {
+                    $users = $this->allUsers();
+                    $authors = $this->project->authors ? $this->project->authors : [];
+                    $projectIdentifier = $this->getIdentifier($this->project, 'identifier');
+                    $suffix = 'P'.$projectIdentifier.'.'.'S'.$identifier;
+                    $url = $url.'S'.$identifier;
+                } elseif ($this instanceof Dataset) {
+                    $users = $this->study->allUsers();
+                    $authors = $this->project->authors ? $this->project->authors : [];
+                    $projectIdentifier = $this->getIdentifier($this->project, 'identifier');
+                    $studyIdentifier = $this->getIdentifier($this->study, 'identifier');
+                    $suffix = 'P'.$projectIdentifier.'.'.'S'.$studyIdentifier.'.'.'D'.$identifier;
+                    $url = $url.'D'.$identifier;
+                }
 
-            $contributors = [];
-            foreach ($authors as $user) {
-                array_push($contributors, [
-                    'name' => $user->family_name.' '.$user->given_name,
-                    'nameType' => 'Personal',
-                    'contributorType' => 'RightsHolder',
-                    'givenName' => $user->given_name,
-                    'familyName' => $user->family_name,
-                    'affiliation' => [],
-                    'nameIdentifiers' => [],
-                ]);
-            }
+                $creators = [];
+                foreach ($users as $user) {
+                    array_push($creators, [
+                        'name' => $user->last_name.' '.$user->first_name,
+                        'nameType' => 'Personal',
+                        'givenName' => $user->first_name,
+                        'familyName' => $user->last_name,
+                        'affiliation' => [],
+                        'nameIdentifiers' => [],
+                    ]);
+                }
 
-            $attributes = [
-                'titles' => [
-                    [
-                        'title' => $this->name,
+                $contributors = [];
+                foreach ($authors as $user) {
+                    array_push($contributors, [
+                        'name' => $user->family_name.' '.$user->given_name,
+                        'nameType' => 'Personal',
+                        'contributorType' => 'RightsHolder',
+                        'givenName' => $user->given_name,
+                        'familyName' => $user->family_name,
+                        'affiliation' => [],
+                        'nameIdentifiers' => [],
+                    ]);
+                }
+
+                $attributes = [
+                    'titles' => [
+                        [
+                            'title' => $this->name,
+                        ],
                     ],
-                ],
-                'url' => $url,
-                'creators' => $creators,
-                'contributors' => $contributors,
-                'isActive' => true,
-                'event' => 'publish',
-                'state' => 'findable',
-                'schemaVersion' => 'http://datacite.org/schema/kernel-4',
-            ];
+                    'url' => $url,
+                    'creators' => $creators,
+                    'contributors' => $contributors,
+                    'isActive' => true,
+                    'event' => 'publish',
+                    'state' => 'findable',
+                    'schemaVersion' => 'http://datacite.org/schema/kernel-4',
+                ];
 
-            $doiResponse = $doiService->createDOI($suffix, $attributes);
-            $this->doi = $doiResponse['data']['id'];
-            $this->datacite_schema = $doiResponse;
-            $this->save();
+                $doiResponse = $doiService->createDOI($suffix, $attributes);
+                $this->doi = $doiResponse['data']['id'];
+                $this->datacite_schema = $doiResponse;
+                $this->save();
+            }
         }
     }
 
