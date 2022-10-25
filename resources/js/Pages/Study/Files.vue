0372b1d19e4d180f376d6571f2105764ff3e9c04
NRayya <82588017+NRayya@users.noreply.github.com>
2022-10-25 16:37:44 +0200

    fix:  null fields sent to datacite Fixes #538

--- resources/js/Pages/Study/Files.vue
+++ resources/js/Pages/Study/Files.vue
@@ -373,7 +373,9 @@ export default {
     mounted() {
         const vm = this;
         vm.$page.props.selectedFileSystemObject = vm.file;
-        vm.$page.props.selectedFolder = vm.file.children[0] ? vm.file.children[0].relative_url : '';
+        vm.$page.props.selectedFolder = vm.file.children[0]
+            ? vm.file.children[0].relative_url
+            : "";
         if (!this.study.is_public) {
             let options = {
                 url: "/",
