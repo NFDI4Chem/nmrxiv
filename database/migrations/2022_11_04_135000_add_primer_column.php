653fd0820d613e12996667f4bc490ed93a0d7146
Chandu Nainala <mailcs76@gmail.com>
2022-11-08 14:51:19 +0100

    Update 2022_11_04_135000_add_primer_column.php

--- database/migrations/2022_11_04_135000_add_primer_column.php
+++ database/migrations/2022_11_04_135000_add_primer_column.php
@@ -25,6 +25,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function($table) {
+           $table->dropColumn('primed');
+        });
     }
 };
