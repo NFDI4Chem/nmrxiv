dc79ea1edd58239a3303c805e5e931cc93c2fe28
NishaSharma14 <sharmanis14@gmail.com>
2022-11-15 16:51:59 +0100

    refactor: author code

--- resources/js/Mixins/Global.js
+++ resources/js/Mixins/Global.js
@@ -106,6 +106,9 @@ export default {
                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(email);
         },
+        isEmpty(obj) {
+            return Object.keys(obj).length === 0;
+        },
     },
 
     computed: {
--- resources/js/Shared/ManageAuthor.vue
+++ resources/js/Shared/ManageAuthor.vue
@@ -572,21 +572,7 @@
                 v-if="authors.length == 0 && !displayAddAuthorForms"
             >
                 <div class="text-center">
-                    <svg
-                        class="mx-auto h-12 w-12 text-gray-400"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                    >
-                        <path
-                            vector-effect="non-scaling-stroke"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-                        />
-                    </svg>
+                    <FolderPlusIcon class="mx-auto h-12 w-12 text-gray-400" />
                     <h3 class="mt-2 text-sm font-medium text-gray-900">
                         No Authors Listed
                     </h3>
@@ -600,17 +586,7 @@
                             class="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
                             <!-- Heroicon name: mini/plus -->
-                            <svg
-                                class="-ml-1 mr-2 h-5 w-5"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"
-                                />
-                            </svg>
+                            <PlusIcon class="w-5 h-5 mr-1 text-white" />
                             Add Author
                         </button>
                     </div>
@@ -702,6 +678,7 @@ import {
     PencilIcon,
     ArrowSmallRightIcon,
     PlusIcon,
+    FolderPlusIcon,
 } from "@heroicons/vue/24/solid";
 import JetInputError from "@/Jetstream/InputError.vue";
 import LoadingButton from "@/Shared/LoadingButton.vue";
@@ -718,6 +695,7 @@ export default {
         JetButton,
         PencilIcon,
         ArrowSmallRightIcon,
+        FolderPlusIcon,
         PlusIcon,
         TrashIcon,
         JetInputError,
