e74df7c16bbc2acf8c0b7f76250b5e3f866c0fa4
NRayya <82588017+NRayya@users.noreply.github.com>
2022-11-21 15:14:06 +0100

    feat: create factories for different models project & validation #598

---
+++ database/factories/ValidationFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use App\Models\Validation;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ValidationFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Validation::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+
+    public function definition()
+    {
+        $dt = Carbon::now();
+
+        return
+        [
+            'report' => $this->faker->sentence(), #todo: adjust the structure of the outout to real report values
+            'score' => 0, #todo: provide varying scores
+            'created_at' => $dt->subDays(rand(1, 10)),
+            'updated_at' => $dt->subHours(rand(1, 10)),
+        ];
+
+    }
+}
