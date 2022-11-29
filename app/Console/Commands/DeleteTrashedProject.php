1bbc560fdfa32cab62bd786f444e61aab6da48fb
NishaSharma14 <sharmanis14@gmail.com>
2022-11-29 15:02:20 +0100

    Collection

--- app/Console/Commands/DeleteTrashedProject.php
+++ app/Console/Commands/DeleteTrashedProject.php
@@ -37,12 +37,12 @@ class DeleteTrashedProject extends Command
             $dueDate = null;
             if ($deletedOn) {
                 $dueDate = Carbon::parse($deletedOn)->diffInDays(Carbon::now());
-                echo 'Due Date  '.$dueDate;
+                //echo 'Due Date  '.$dueDate;
                 if ($dueDate == 23 || $dueDate == 29) {
                     $this->sendNotification($project);
                 }
                 if ($dueDate >= 30) {
-                    $this->deletePermanently($dueDate, $project);
+                    $this->deleteObjects($project);
                 }
             }
         }
@@ -69,13 +69,139 @@ class DeleteTrashedProject extends Command
     }
 
     /**
-     * Delete project and related studies and datasets permanently.
+     * Delete project and related objects permanently.
      *
      * @param  string  $owner
      * @param  App\Models\Project  $project
      * @return void
      */
-    public function deletePermanently($dueDate, $project)
+    public function deleteObjects($project)
     {
+        echo 'Deletion starts..';
+        if (! $project->is_public) {
+            $this->deleteDraftAndFiles($project);
+            $this->deleteStudiesAndRelations($project);
+        } else {
+            echo 'Project-'.$project->name.' cannot be deleted as it is public.';
+        }
+    }
+
+    /**
+     * Delete drafts and associated files permanently.
+     *
+     * @param  App\Models\Draft  $drafts
+     * @return void
+     */
+    public function deleteDraftAndFiles($project)
+    {
+        $draft = $project->draft;
+        if ($draft) {
+            //Delete Files
+            $files = $draft->files;
+            echo 'Delete Files..';
+            foreach ($files as $file) {
+                $file->delete();
+            }
+            //Delete Draft
+            $draft = $draft->refresh();
+            if ($draft->files->isEmpty()) {
+                echo 'Delete Drafts..';
+                $draft->delete();
+            }
+        }
+    }
+
+    /**
+     * Delete study and related objects permanently.
+     *
+     * @param  App\Models\Project  $project
+     * @return void
+     */
+    public function deleteStudiesAndRelations($project)
+    {
+        $studies = $project->studies;
+        foreach ($studies as $study) {
+            $datasets = $study->datasets;
+            $sample = $study->sample;
+            //Delete Sample and Detach Molecules
+            if ($sample) {
+                $molecules = $sample->molecules;
+                if ($molecules) {
+                    foreach ($molecules as $molecule) {
+                        $sample->molecules()->detach([$molecule->id]);
+                    }
+                }
+                $sample->delete();
+            }
+            if ($datasets && count($datasets) > 0) {
+                $this->deleteDatasetsAndRelations($study);
+            }
+        }
+        $this->deleteProjectAndRelations($project);
+    }
+
+    /**
+     * Delete dataset and related objects permanently.
+     *
+     * @param  \Illuminate\Support\Collection  $datasets
+     * @return void
+     */
+    public function deleteDatasetsAndRelations($study)
+    {
+        $datasets = $study->datasets;
+        foreach ($datasets as $dataset) {
+            $nmriumInfo = $dataset->nmrium();
+            //Delete NMRium
+            if ($nmriumInfo) {
+                echo 'Delete NMRium';
+                $nmriumInfo->delete();
+            }
+            //Delete Dataset
+            $dataset->delete();
+            echo 'Delete Dataset..';
+        }
+
+        //Delete Study
+        $study = $study->fresh();
+        if ($study->datasets->isEmpty()) {
+            echo 'Delete Study..';
+            $study->delete();
+        }
+    }
+
+    /**
+     * Delete project and related objects permanently.
+     *
+     * @param  App\Models\Project  $project
+     * @return void
+     */
+    public function deleteProjectAndRelations($project)
+    {
+        $project = $project->fresh();
+        $authors = $project->authors;
+        $citations = $project->citations;
+        //Detach Authors
+        if ($authors) {
+            foreach ($authors as $author) {
+                $project->authors()->detach(
+                    [$author->id]
+                );
+            }
+        }
+        //Detach Citations
+        if ($citations) {
+            foreach ($citations as $citation) {
+                $project->citations()->detach(
+                    [$citation->id]
+                );
+            }
+        }
+
+        //Delete Project
+        $project = $project->fresh();
+        if ($project->studies->isEmpty()) {
+            echo 'Delete Project..';
+            $project->delete();
+        }
     }
 }
