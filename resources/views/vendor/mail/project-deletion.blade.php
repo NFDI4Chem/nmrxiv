fddde440ccc9252ae6e4bf891a337d0ae7084009
NishaSharma14 <sharmanis14@gmail.com>
2022-12-02 13:43:18 +0100

    fix: update project-delete mail template

--- resources/views/vendor/mail/project-deletion.blade.php
+++ resources/views/vendor/mail/project-deletion.blade.php
@@ -2,14 +2,20 @@
 
 Dear nmrXiv user,
 
-{{ __('Your project - :project has been moved to trash successfully on :deletedOn. It will stay in trash until the cool-off period of 30 days after which it will be deleted permanently.', ['project' => $projectName, 'deletedOn' => $deletedOn]) }}
+{{ __('Your project - :project was deleted on :deletedOn.', ['project' => $projectName, 'deletedOn' => $deletedOn]) }}
 
-You can always restore your project anytime before the cool-off period. 
-To restore:
-Go to your Trash section click on Project >> Project Settings >> Restore Project,  or click on the button below.
+You may recover deleted projects and the data stored in them for a limited time before they are permanently deleted.
+{{ __('If you had like to recover your project, you must cancel the project permanent deletion before :dueDate ', ['dueDate' => $dueDate]) }}
 
 
-If you do not recognize this action please report to us at info@nmrxiv.org.
+To recover your deleted project:
+Visit the thrash page (click the button below).
+Select the project you want to recover, and click Project settings > Restore.
+In the confirmation dialogue, click Restore.
+
+{{ __('If you take no action by :dueDate, you will be unable to recover your project.', ['dueDate' => $dueDate]) }}
+
+For further questions, please visit our documentation site or contact us at info@nmrxiv.org.
 
 @component('mail::button', ['url' =>  $url, 'color' => 'green'])
 Restore Project
