deec1250717f0af8ed623ad75b26504941c1e471
NishaSharma14 <sharmanis14@gmail.com>
2022-12-06 14:21:03 +0100

    fix: add sender's details in study invite mail

--- app/Mail/StudyInvitation.php
+++ app/Mail/StudyInvitation.php
@@ -2,12 +2,13 @@
 
 namespace App\Mail;
 
-use App\Models\StudyInvitation as StudyInvitationModel;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Models\StudyInvitation as StudyInvitationModel;
 
 class StudyInvitation extends Mailable implements ShouldQueue
 {
@@ -38,7 +39,10 @@ class StudyInvitation extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        return $this->markdown('vendor.mail.study-invitation', ['acceptUrl' => URL::signedRoute('study-invitations.accept', [
+        dd($this->invitation);
+        $invitedBy = User::find($this->invitation->invited_by);
+        
+        return $this->markdown('vendor.mail.study-invitation', ['invitedBy' => $invitedBy ? $invitedBy->name : "", 'acceptUrl' => URL::signedRoute('study-invitations.accept', [
             'invitation' => $this->invitation,
         ])])->subject(__('Study Invitation'));
     }
