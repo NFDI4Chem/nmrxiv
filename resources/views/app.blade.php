c074b5eed89c3d914bd28c8e2c8e29a8079c9a03
NishaSharma14 <sharmanis14@gmail.com>
2022-12-26 13:28:48 +0100

    fix: update matomo configuration

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,8 +16,8 @@
         @routes
         
         @env ('production')
-            <!-- Matomo -->
-            <script>
+            <!-- Matomo -->																					|
+            <script>	
             var _paq = window._paq = window._paq || [];
             /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
             _paq.push(['trackPageView']);
@@ -25,7 +25,7 @@
             (function() {
                 var u="//matomo.nmrxiv.org/";
                 _paq.push(['setTrackerUrl', u+'matomo.php']);
-                _paq.push(['setSiteId', '2']);
+                _paq.push(['setSiteId', '1']);
                 var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
                 g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
             })();
