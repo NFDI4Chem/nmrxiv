ad8da605edcd2890834228c9d13d75e04bff6d5c
NishaSharma14 <sharmanis14@gmail.com>
2022-12-29 12:01:30 +0100

    fix: review changes

--- app/Mail/TeamInvitation.php
+++ app/Mail/TeamInvitation.php
@@ -3,7 +3,6 @@
 namespace App\Mail;
 
 use App\Models\TeamInvitation as TeamInvitationModel;
-use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -39,9 +38,7 @@ class TeamInvitation extends Mailable implements ShouldQueue
      */
     public function build()
     {
-        $invitedBy = User::find($this->invitation->invited_by);
-
-        return $this->markdown('vendor.mail.team-invitation', ['invitedBy' => $invitedBy ? $invitedBy->name : '', 'acceptUrl' => URL::signedRoute('team-invitations.accept', [
+        return $this->markdown('vendor.mail.team-invitation', ['acceptUrl' => URL::signedRoute('team-invitations.accept', [
             'invitation' => $this->invitation,
         ])])->subject(__('Team Invitation'));
     }
