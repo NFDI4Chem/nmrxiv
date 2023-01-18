c05870e9e279f87c32abdaa1a125a98452138269
NishaSharma14 <sharmanis14@gmail.com>
2023-01-18 15:11:09 +0100

    console.users.destroy-photo

---
+++ app/Notifications/TeamInvitationNotification.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\TeamInvitation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+class TeamInvitationNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    private $invitation;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(TeamInvitation $invitation)
+    {
+        $this->invitation = $invitation;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        $message = 'You have been invited to join the '.$this->invitation->team->name.' team (role '.$this->invitation['role'].') by '.$this->invitation['invited_by'].'. Please check your mail to accept or decline the invitation.';
+
+        return [
+            'title' => 'Team Invitation',
+            'message' => $message,
+        ];
+    }
+}
