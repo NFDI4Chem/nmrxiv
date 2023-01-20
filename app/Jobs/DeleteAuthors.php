59aec0901dae71c56b5249d7e3de1a878af7bc3e
NRayya <82588017+NRayya@users.noreply.github.com>
2023-01-20 08:33:42 +0100

    fix: pint/format

--- app/Jobs/DeleteAuthors.php
+++ app/Jobs/DeleteAuthors.php
@@ -3,11 +3,11 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class DeleteAuthors implements ShouldQueue
 {
