0a67398bfb4e0623f498835371731a472a7dafdc
NRayya <82588017+NRayya@users.noreply.github.com>
2023-05-03 13:09:32 +0200

    refactor: switch to JetStream buttons, Announcements Fixes #450

--- resources/js/Pages/Announcement/Index.vue
+++ resources/js/Pages/Announcement/Index.vue
@@ -220,30 +220,18 @@
                 </div>
             </span>
             <span v-else>
-                <button
-                    type="button"
-                    class="relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                <jet-secondary-button
+                    class="block w-fit center border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     @click="openAnnouncementCreateDialog()"
                 >
-                    <svg
-                        class="mx-auto h-12 w-12 text-gray-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        stroke="currentColor"
-                        fill="none"
-                        viewBox="0 0 48 48"
+                    <BellAlertIcon
+                        class="mx-auto h-40 w-12 text-gray-400"
                         aria-hidden="true"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-                        />
-                    </svg>
-                    <span class="mt-2 block text-sm font-medium text-gray-900">
+                    />
+                    <span class="mx-auto text-sm font-medium text-gray-900">
                         Create a new announcement
                     </span>
-                </button>
+                </jet-secondary-button>
             </span>
             <announcement-create
                 ref="announcementCreateElement"
@@ -268,6 +256,7 @@ import JetDialogModal from "@/Jetstream/DialogModal.vue";
 import { ref, watch } from "vue";
 import { Inertia } from "@inertiajs/inertia";
 import ToggleButton from "@/Shared/ToggleButton.vue";
+import { BellAlertIcon } from "@heroicons/vue/24/solid";
 
 export default {
     components: {
@@ -281,6 +270,7 @@ export default {
         JetSecondaryButton,
         JetDialogModal,
         ToggleButton,
+        BellAlertIcon,
     },
     props: {
         announcements: Array,
