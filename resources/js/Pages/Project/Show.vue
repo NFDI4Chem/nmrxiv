6461bafbc80a1040ebffa7ad10cc299af652af77
NRayya <82588017+NRayya@users.noreply.github.com>
2023-08-01 12:47:10 +0200

    add info icon for avatars

--- resources/js/Pages/Project/Show.vue
+++ resources/js/Pages/Project/Show.vue
@@ -89,6 +89,10 @@
                                     called-from="projectView"
                                     model="project"
                                 />
+                                <ToolTip
+                                    class="inline h-4 w-4 ml-0"
+                                    text="The avatars (left) correspond to the users with whom the project is shared. Click there to edit sharing options."
+                                ></ToolTip>
                                 <a
                                     class="cursor-pointer hover:text-teal-900 inline-flex items-center ml-7"
                                     @click="toggleDetails"
