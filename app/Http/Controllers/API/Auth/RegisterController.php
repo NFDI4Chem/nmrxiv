aa25b20708328740a657abb5566f297497fa6061
NishaSharma14 <sharmanis14@gmail.com>
2023-11-30 15:35:28 +0100

    fix: add swagger doc for auths and public projects

--- app/Http/Controllers/API/Auth/RegisterController.php
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -6,33 +6,77 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    /**
+     * @OA\Post(
+     * path="/api/v1/auth/register",
+     * summary="Register",
+     * description="Register by providing details.",
+     * operationId="authRegister",
+     * tags={"auth"},
+     *
+     * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass registration details.",
+     *
+     *    @OA\JsonContent(
+     *       required={"first_name","last_name","email","password","username"},
+     *
+     *       @OA\Property(property="first_name", type="string", format="first_name", example="Nisha"),
+     *       @OA\Property(property="last_name", type="string", format="last_name", example="Sharma"),
+     *       @OA\Property(property="email", type="string", format="email", example="nisha.sharma@email.com"),
+     *       @OA\Property(property="username", type="string", format="username", example="nis123"),
+     *       @OA\Property(property="orcid_id", type="string", format="orcid_id", example="0009-0006-4755-1039"),
+     *       @OA\Property(property="password", type="string", format="password", example="secret1234"),
+     *
+     *    ),
+     * ),
+     *
+     * @OA\Response(
+     *    response=201,
+     *    description="Successful Operation"
+     *    ),
+     * @OA\Response(
+     *    response=422,
+     *    description="Unprocessable Content"
+     * )
+     * )
+     */
     public function register(Request $request): JsonResponse
     {
         $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
-            'orcid_id' => string,
-            'affiliation' => string,
+            'username' => 'required|string',
         ]);
 
         $user = User::create([
-            'name' => $validatedData['name'],
+            'name' => $validatedData['first_name'].' '.$validatedData['last_name'],
+            'first_name' => $validatedData['first_name'],
+            'last_name' => $validatedData['last_name'],
             'email' => $validatedData['email'],
-            'orcid_id' => $validatedData['orcid_id'],
-            'affiliation' => $validatedData['affiliation'],
+            'username' => $validatedData['username'],
+            'orcid_id' => $request['orcid_id'],
+            'affiliation' => $request['affiliation'],
             'password' => Hash::make($validatedData['password']),
         ]);
+
+        $user->sendEmailVerificationNotification();
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
+            'success' => true,
+            'message' => 'User creation successful. Kindly confirm your email address by clicking the link sent to your inbox.',
             'access_token' => $token,
             'token_type' => 'Bearer',
-        ]);
+        ],
+            201);
     }
 }
--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,6 +9,24 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    /**
+     * @OA\Get(
+     * path="/api/v1/projects",
+     * summary="Fetch all public projects",
+     * description="Fetch details for all publicly available projects on nmrXiv.",
+     * operationId="publicProjects",
+     * tags={"public"},
+     *
+     * @OA\Response(
+     *    response=200,
+     *    description="Successful Operation"
+     *    ),
+     * @OA\Response(
+     *    response=500,
+     *    description="Internal Server Error"
+     * )
+     * )
+     */
     public function all(Request $request)
     {
         $sort = $request->get('sort');
@@ -20,5 +38,46 @@ class ProjectController extends Controller
         }
 
         return ProjectResource::collection(Project::where('is_public', true)->paginate(15));
+    }
+
+    /**
+     * @OA\Get(
+     * path="/api/v1/project/?id={id}",
+     * summary="Fetch nmrXiv public project based on identifier",
+     * description="Fetch details of nmrXiv public project based on identifier.",
+     * operationId="publicProject",
+     * tags={"public"},
+     *
+     * @OA\Parameter(
+     *  name="id",
+     *  in="query",
+     *  description="Public project identifier e.g. P12",
+     *  required=true,
+     *
+     *      @OA\Schema(
+     *          type="string",
+     *    )
+     * ),
+     *
+     * @OA\Response(
+     *    response=200,
+     *    description="Successful Operation"
+     *    ),
+     * @OA\Response(
+     *    response=500,
+     *    description="Internal Server Error"
+     * )
+     * )
+     */
+    public function id(Request $request)
+    {
+        $id = $request->query('id');
+
+        if ($id) {
+            return $project = Project::where([
+                ['is_public', true],
+                ['identifier', str_replace('P', '', $id)],
+            ])->get();
+        }
     }
 }
