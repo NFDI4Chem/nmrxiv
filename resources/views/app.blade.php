f21d3935489d68010c258b949f2ba66822e50b61
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-12-15 09:15:50 +0100

    fix: various minor updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
         <title inertia>{{ config('app.name', 'nmrXiv') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"> -->
 
         <!-- Styles / Scripts -->
         @vite(['resources/js/app.js'])
