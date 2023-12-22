a8425be2aa04aef4b1daf03132e34fdad439899d
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2023-12-22 15:24:05 +0000

    fix: Added recommended journals to welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -316,6 +316,47 @@
                 </div>
             </div>
 
+            <div class="border-b">
+                <div class="bg-white pb-24 pt-24">
+                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                        <h2
+                            class="text-center text-lg font-semibold leading-8 text-gray-900"
+                        >
+                            Recommended by leading journals as the
+                            community-trusted repository for NMR data deposition
+                        </h2>
+                        <div
+                            class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4"
+                        >
+                            <img
+                                class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                alt=""
+                            />
+                            <a
+                                href="https://pubs.acs.org/doi/10.1021/acs.jnatprod.3c00281"
+                                target="_blank"
+                                ><img
+                                    class="col-span-2 w-full object-contain lg:col-span-1"
+                                    src="/img/angewandte-chemie.png"
+                                    alt="Angewandte Chemie"
+                            /></a>
+                            <a
+                                href="https://onlinelibrary.wiley.com/page/journal/15213773/homepage/notice-to-authors"
+                                target="_blank"
+                                ><img
+                                    class="col-span-2 w-full object-contain lg:col-span-1"
+                                    src="/img/jnp.png"
+                                    alt="Journal of Natural Products"
+                            /></a>
+                            <img
+                                class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div class="relative pt-16">
                 <div>
                     <div
