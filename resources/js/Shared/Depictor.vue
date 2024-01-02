7207c753c9749cc215b7462f49419709c9f3c98b
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2024-01-02 23:31:07 +0000

    fix: fixes #978

--- resources/js/Shared/Depictor.vue
+++ resources/js/Shared/Depictor.vue
@@ -22,7 +22,7 @@
                         :key="tab"
                         :class="[
                             selectedTab == tab ? 'bg-gray-500 text-white' : '',
-                            'border hover:cursor-pointer text-gray-500 hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium',
+                            'border hover:cursor-pointer hover:text-gray-700 rounded-md px-3 py-2 text-sm font-medium',
                         ]"
                         @click="selectedTab = tab"
                     >
