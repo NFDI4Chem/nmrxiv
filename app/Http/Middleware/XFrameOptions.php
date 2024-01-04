d1080b099c7b2dbd714a56a8e37944ebe751276c
Venkata Chandra Sekhar Nainala <mailcs76@gmail.com>
2024-01-04 14:22:34 +0000

    fix: frame src enabled

--- app/Http/Middleware/XFrameOptions.php
+++ app/Http/Middleware/XFrameOptions.php
@@ -18,7 +18,7 @@ class XFrameOptions
         $response = $next($request);
 
         if ($request->route()->getName() == 'embed') {
-            return $response->header('Content-Security-Policy', "default-src 'self'; base-uri 'self'; block-all-mixed-content; frame-src data: blob: *; img-src 'self'; style-src 'unsafe-inline' *;");
+            return $response->header('Content-Security-Policy', 'frame-src data: blob: *');
         } else {
             $xframeOptions = 'SAMEORIGIN';
 
