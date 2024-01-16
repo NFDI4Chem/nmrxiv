d3b065cf70f5044c99dde38e49c5a373db1f7aa1
NishaSharma14 <sharmanis14@gmail.com>
2024-01-16 13:11:59 +0100

    add cancel button

--- resources/js/Pages/Project/Show.vue
+++ resources/js/Pages/Project/Show.vue
@@ -660,14 +660,14 @@
 
                                         <div>
                                             <div class="px-8 pb-8 pt-0">
-                                                <button
+                                                <jet-success-button
                                                     type="button"
                                                     :class="[
-                                                        !project.terms ||
+                                                        !project.terms &&
                                                         !project.conditions
                                                             ? 'bg-gray-200 cursor-not-allowed'
                                                             : 'bg-green-600 hover:bg-green-700',
-                                                        'inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 sm:w-auto sm:text-sm',
+                                                        'ml-2',
                                                     ]"
                                                     @click="
                                                         showPublishConfirmationModal = true
@@ -678,14 +678,21 @@
                                                     "
                                                 >
                                                     Publish now
-                                                </button>
-                                                <button
+                                                </jet-success-button>
+                                                <jet-secondary-button
                                                     type="button"
-                                                    class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                                                    class="ml-2"
                                                     @click="updatePublishDate()"
                                                 >
                                                     Update publish date
-                                                </button>
+                                                </jet-secondary-button>
+                                                <jet-secondary-button
+                                                    type="button"
+                                                    class="ml-2"
+                                                    @click="showPublishDialog=false"
+                                                >
+                                                    Cancel
+                                                </jet-secondary-button>
                                             </div>
                                             <div v-if="errors">
                                                 <div
