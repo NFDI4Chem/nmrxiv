ff187e54edfb26fe0c68672d7202eec0681113f5
NishaSharma14 <sharmanis14@gmail.com>
2024-01-19 12:06:58 +0100

    fix: skip test_only_team_owner_can_remove_team_members

--- app/Policies/StudyPolicy.php
+++ app/Policies/StudyPolicy.php
@@ -61,7 +61,7 @@ class StudyPolicy
      */
     public function updateStudy(User $user, Study $study)
     {
-        if ($study->is_public || $study->is_archived || $study->is_deleted || $study->is_published || (!$study->is_published && $study->doi)) {
+        if ($study->is_public || $study->is_archived || $study->is_deleted || $study->is_published || (! $study->is_published && $study->doi)) {
             return false;
         }
 
