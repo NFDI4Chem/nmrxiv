de83de6041434e0830cfda0fc9968f6fe081d090
NRayya <82588017+NRayya@users.noreply.github.com>
2024-07-30 10:44:25 +0200

    fix: update DataCite Meta data for unpublished projects fixes #1143

---
+++ app/Console/Commands/UpdateProjectsDOI.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Project;
+use App\Services\DOI\DOIService;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class UpdateProjectsDOI extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nmrxiv:update-projects-dois {operator?} {update-date?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update DataCite DOI metadata of all the projects updated before or after a given date.';
+
+    /**
+     * The DOI service instance.
+     *
+     * @var \App\Services\DOI\DOIService
+     */
+    protected $doiService;
+
+    /**
+     * Create a new class instance.
+     *
+     * @return void
+     */
+    public function __construct(DOIService $doiService)
+    {
+        parent::__construct();
+
+        // Assign the DOI service instance
+        $this->doiService = $doiService;
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // $operator takes values from
+        // {'>' : after a certain update date,
+        //  '<' : before a certain update date,
+        //  '=' : at a certain update date}
+        $operator = $this->argument('operator');
+        $date = $this->argument('update-date');
+        $date = $date ? Carbon::parse($date) : null;
+
+        // Fetch the projects based on the date
+        $projects = $date ? Project::where('updated_at', $operator, $date)->get() : Project::all();
+        foreach ($projects as $project) {
+            echo $project->identifier."\r\n";
+            foreach ($project->studies as $study) {
+                echo $study->identifier."\r\n";
+                foreach ($study->datasets as $dataset) {
+                    echo $dataset->identifier."\r\n";
+                    $dataset->updateDOIMetadata($this->doiService);
+                    $dataset->addRelatedIdentifiers($this->doiService);
+                }
+                $study->updateDOIMetadata($this->doiService);
+                $study->addRelatedIdentifiers($this->doiService);
+            }
+            $project->updateDOIMetadata($this->doiService);
+            $project->addRelatedIdentifiers($this->doiService);
+        }
+    }
+}
