928561686d76d6858ae814c77834173bcef28deb
Shift <shift@laravelshift.com>
2025-03-11 12:36:18 +0000

    Adopt anonymous migrations

--- database/migrations/2022_06_25_081954_create_tag_tables.php
+++ database/migrations/2022_06_25_081954_create_tag_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTagTables extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -38,4 +38,4 @@ class CreateTagTables extends Migration
         Schema::dropIfExists('taggables');
         Schema::dropIfExists('tags');
     }
-}
+};
