e666c3d9b691ebaafd02b587f9aabb82cbf3438b
Shift <shift@laravelshift.com>
2025-03-11 12:36:15 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Lab404\Impersonate\Events\LeaveImpersonation;
+use Lab404\Impersonate\Events\TakeImpersonation;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     /**
      * Register any application services.
      *
@@ -30,5 +42,24 @@ class AppServiceProvider extends ServiceProvider
         if (App::environment('production')) {
             \URL::forceScheme('https');
         }
+
+        $this->bootEvent();
+    }
+
+    public function bootEvent()
+    {
+        Event::listen(function (TakeImpersonation $event) {
+            session()->put([
+                'password_hash_sanctum' => $event->impersonated->getAuthPassword(),
+            ]);
+        });
+
+        Event::listen(function (LeaveImpersonation $event) {
+            session()->remove('password_hash_web');
+            session()->put([
+                'password_hash_sanctum' => $event->impersonator->getAuthPassword(),
+            ]);
+            Auth::setUser($event->impersonator);
+        });
     }
 }
