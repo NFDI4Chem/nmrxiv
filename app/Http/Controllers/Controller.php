bf884524b2359ab7a17ed3ceb891a41c26263cf4
Shift <shift@laravelshift.com>
2025-03-11 12:46:07 +0000

    Remove unused `AuthorizesRequests` trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -21,5 +20,5 @@ use Illuminate\Routing\Controller as BaseController;
  */
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs;
+    use DispatchesJobs;
 }
