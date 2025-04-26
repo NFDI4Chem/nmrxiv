e4f5cb74964eed932247d301d7666d8ee9ed813c
Venkata Nainala <mailcs76@gmail.com>
2025-04-26 13:01:02 +0200

    fix: resolved migration issue and started exposing pgsql

--- database/migrations/2024_01_25_142911_rename_url_to_obfuscation_code_datasets.php
+++ database/migrations/2024_01_25_142911_rename_url_to_obfuscation_code_datasets.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('datasets', function (Blueprint $table) {
-            $table->renameColumn('url', 'obfuscationCode');
+            $table->renameColumn('obfuscationCode', 'url');
         });
     }
 };
