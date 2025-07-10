988b96651eea6a3f7ddccef1abe3f589bffd9180
NishaSharma14 <nisha.sharma@uni-jena.de>
2025-07-10 10:24:49 +0200

    fix: enhance deploy script

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -42,11 +42,40 @@ export HTTPS_PROXY="${HTTPS_PROXY:-$https_proxy}"
 export NO_PROXY="${NO_PROXY:-$no_proxy}"
 export COMPOSE_PROJECT_NAME=nmrxiv-dev
 
+# === Validate Environment Variables ===
+validate_env_vars() {
+    local missing_vars=()
+
+    for var in DB_USERNAME DB_DATABASE; do
+        if [ -z "${!var}" ]; then
+            missing_vars+=("$var")
+        fi
+    done
+
+    if [ ${#missing_vars[@]} -ne 0 ]; then
+        log_message "❌ Missing required environment variables: ${missing_vars[*]}"
+        exit 1
+    fi
+}
+
+# Call the validation function at the start of the script
+validate_env_vars
+
 # === Functions ===
 check_health() {
-    HEALTH=$(docker inspect --format='{{json .State.Health.Status}}' "$NEW_CONTAINER_ID")
+    if ! HEALTH=$(docker inspect --format='{{json .State.Health.Status}}' "$NEW_CONTAINER_ID" 2>/dev/null); then
+        log_message "❌ Failed to inspect container $NEW_CONTAINER_ID. Ensure the container ID is correct."
+        return 1
+    fi
+
     log_message "Health status for $NEW_CONTAINER_ID: $HEALTH"
-    [[ "$HEALTH" == *"healthy"* ]]
+
+    if [[ "$HEALTH" == *"healthy"* ]]; then
+        return 0
+    else
+        log_message "❌ Container $NEW_CONTAINER_ID is not healthy."
+        return 1
+    fi
 }
 
 wait_for_health() {
@@ -98,6 +127,7 @@ cleanup() {
 }
 
 deploy_service() {
+    log_message "Starting zero-downtime deployment..."
     local service=$1
     local image=$2
     local run_healthcheck=$3
@@ -113,16 +143,17 @@ deploy_service() {
         NEW_CONTAINER_ID=$(docker ps -q -l)
         log_message "🔍 New container ID: $NEW_CONTAINER_ID"
 
-        if wait_for_health; then
-            remove_old_containers "$service"
-            log_message "✅ Deployment of $service done successfully.."
-            run_migration_and_clear_cache
-            log_message "Application is available at: https://dev.nmrxiv.org"
-        else
-            log_message "❌ Deployment aborted: new $service container is unhealthy."
-            docker stop "$NEW_CONTAINER_ID"
-            docker rm "$NEW_CONTAINER_ID"
-        fi
+        # Skipping health check for dev because we want the service to be down if there is an error in the container
+        # if wait_for_health; then
+        #     remove_old_containers "$service"
+        #     log_message "✅ Deployment of $service done successfully.."
+        #     run_migration_and_clear_cache
+        #     log_message "Application is available at: https://dev.nmrxiv.org"
+        # else
+        #     log_message "❌ Deployment aborted: new $service container is unhealthy."
+        #     docker stop "$NEW_CONTAINER_ID"
+        #     docker rm "$NEW_CONTAINER_ID"
+        # fi
     else
         log_message "✅ No update for $service Skipping deployment."
     fi
@@ -182,6 +213,57 @@ build_multi_platform() {
     log_message "✅ Multi-platform builds completed successfully!"
 }
 
+check_db_readiness() {
+    log_message "Waiting for database to be ready..."
+    for i in {1..10}; do
+        if docker compose -f "$COMPOSE_FILE" exec -T pgsql psql -U "${DB_USERNAME}" -d "${DB_DATABASE}" -c "SELECT 1" >/dev/null 2>&1; then
+            log_message "✅ Database is ready."
+            return 0
+        else
+            log_message "Retry $i/10: Waiting 10s for database to be ready..."
+            sleep 10
+        fi
+
+        if [ $i -eq 10 ]; then
+            log_message "❌ Database readiness check failed after 10 retries."
+            return 1
+        fi
+    done
+}
+
+build_or_restart_services() {
+    if docker compose -f "$COMPOSE_FILE" ps -q | grep -q .; then 
+        docker compose -f "$COMPOSE_FILE" down --remove-orphans
+    fi
+
+    log_message "Building app containers..."
+    docker compose -f "$COMPOSE_FILE" build --no-cache
+    docker compose -f "$COMPOSE_FILE" up -d
+
+    if ! check_db_readiness; then
+        exit 1
+    fi
+
+    run_migration_and_clear_cache
+
+    cleanup
+    log_message "Services restarted successfully!"
+    log_message "Application is available at: https://dev.nmrxiv.org/"
+}
+
+# === Display Help ===
+display_help() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "\nOptions:"
+    echo "  --build           Build and deploy the application"
+    echo "  --deploy          Perform zero-downtime deployment"
+    echo "  --backup          Create a database backup"
+    echo "  --restart         Restart services"
+    echo "  --multi-platform  Build multi-platform Docker images"
+    echo "  --help            Display this help message"
+    exit 0
+}
+
 # === Parse arguments ===
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -189,42 +271,37 @@ while [[ $# -gt 0 ]]; do
         --deploy) DEPLOY=true; shift ;;
         --backup) BACKUP=true; shift ;;
         --multi-platform) MULTI_PLATFORM=true; shift ;;
+        --restart) RESTART=true; shift ;;
+        --help) HELP=true; shift ;;
         *) log_message "Unknown option: $1"; exit 1 ;;
     esac
 done
 
 # === Deployment Flow ===
-if [ "$MULTI_PLATFORM" = true ]; then
-    build_multi_platform
-    log_message "🎉 Multi-platform build completed successfully!"
-
-elif [ "$DEPLOY" = true ]; then
-    log_message "Starting zero-downtime deployment..."
-
-    deploy_service app "$APP_IMAGE" true
-    deploy_service worker "$WORKER_IMAGE" true
-
-elif [ "$BUILD" = true ]; then
-    docker compose -f "$COMPOSE_FILE" down --remove-orphans
-
-    log_message "Building containers..."
-    docker compose -f "$COMPOSE_FILE" build --no-cache
-    docker compose -f "$COMPOSE_FILE" up -d
-
-    log_message "Waiting for database to be ready..."
-    sleep 10
-
-    run_migration_and_clear_cache
-    
-    cleanup
-    log_message "🎉 Build completed successfully!"
-    log_message "Application is available at: https://dev.nmrxiv.org"
-
-elif [ "$BACKUP" = true ]; then
-    backup_database
-else
-    log_message "Skipping build and deploy step — please pass at least one argument (--build, --deploy, or --multi-platform)..."
-fi
+case true in
+    "$MULTI_PLATFORM")
+        build_multi_platform
+        ;;
+    "$DEPLOY")
+        deploy_service app "$APP_IMAGE" true
+        deploy_service worker "$WORKER_IMAGE" true
+        ;;
+    "$BUILD")
+        build_or_restart_services
+        ;;
+    "$RESTART")
+        build_or_restart_services
+        ;;
+    "$BACKUP")
+        backup_database
+        ;;
+    "$HELP")
+        display_help
+        ;;
+    *)
+        log_message "Skipping build and deploy step — please pass at least one argument: \n--build: Build and deploy the application \n--deploy: Perform zero-downtime deployment \n--backup: Create a database backup \n--restart: Restart services \n--multi-platform: Build multi-platform Docker images. If you are unsure, use the --help flag for guidance."
+        ;;
+esac
 
 # === End of script ===
 log_message "✅ === END of deployment script ==="
