9a1eed10ae90b297b806aff92c5dbbadc1b37a57
Venkata Nainala <mailcs76@gmail.com>
2025-08-21 09:46:24 +0100

    fix: directory missing or present flag default set

--- app/Console/Commands/SanitizeProjects.php
+++ app/Console/Commands/SanitizeProjects.php
@@ -30,7 +30,6 @@ class SanitizeProjects extends Command
      * Execute the console command.
      */
     public function handle()
-    public function handle()
     {
         $name = $this->argument('name');
 
