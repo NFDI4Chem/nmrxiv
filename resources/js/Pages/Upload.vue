c21f5b109e72cfcdb48455fe2ce22a2a6fc2545b
Venkata Nainala <mailcs76@gmail.com>
2025-09-01 15:46:14 +0200

    fix: study name break issue fix

--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -1038,7 +1038,7 @@
                                                         <div>
                                                             <div>
                                                                 <div
-                                                                    class="px-4 py-1.5 -mx-2 bg-gray-50 border-b px-4 py-3 flex items-center font-semibold text-sm text-slate-900 dark:text-slate-200 bg-slate-50/90 dark:bg-slate-700/90 backdrop-blur-sm ring-1 ring-slate-900/10 dark:ring-black/10"
+                                                                    class="px-4 py-1.5 -mx-2 bg-gray-50 border-b px-4 py-3 flex items-center font-semibold text-sm text-slate-900 dark:text-slate-200 bg-slate-50/90 dark:bg-slate-700/90 backdrop-blur-sm ring-1 ring-slate-900/10 dark:ring-black/10 break-all"
                                                                 >
                                                                     <div
                                                                         class="cursor-pointer tooltip m-3 text-gray-500 hover:text-gray-700"
