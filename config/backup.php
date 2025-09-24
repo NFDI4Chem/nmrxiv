bd15426393b58c7bd2a880fdcdd0286ea27192aa
Nisha Sharma <sharmanis14@gmail.com>
2025-09-24 14:25:22 +0200

    fix: update backup config

--- config/backup.php
+++ config/backup.php
@@ -252,7 +252,7 @@ return [
             /*
              * The number of days for which daily backups must be kept.
              */
-            'keep_daily_backups_for_days' => 0,
+            'keep_daily_backups_for_days' => 7,
 
             /*
              * The number of weeks for which one weekly backup must be kept.
