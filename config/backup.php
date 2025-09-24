a5dd9297aede899a2c87d4ef3fab54a1d576c523
Nisha Sharma <sharmanis14@gmail.com>
2025-09-24 13:20:20 +0200

    configure the backup keep days via env

--- config/backup.php
+++ config/backup.php
@@ -247,7 +247,7 @@ return [
             /*
              * The number of days for which backups must be kept.
              */
-            'keep_all_backups_for_days' => 7,
+            'keep_all_backups_for_days' => (int) env('BACKUP_KEEP_DAYS', 7),
 
             /*
              * The number of days for which daily backups must be kept.
