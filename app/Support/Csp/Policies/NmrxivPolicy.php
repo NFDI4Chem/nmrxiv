0079e0976b42dd9cb925e2e4f156c455b68bfe5d
Nisha Sharma <sharmanis14@gmail.com>
2025-10-29 12:01:07 +0100

    fix:remove single policy addition and make multiple additions for font-src directive

--- app/Support/Csp/Policies/NmrxivPolicy.php
+++ app/Support/Csp/Policies/NmrxivPolicy.php
@@ -72,42 +72,40 @@ class NmrxivPolicy implements Preset
     private function addNmrxivSources(Policy $policy): void
     {
         // Image sources - External domains for logos, avatars, and institutional content
-        // Add all image sources in one call (include 'self', data: and blob:)
-        $policy->add(Directive::IMG,
-            Keyword::SELF,
-            'data:',
-            'blob:',
-            'https://www.uni-jena.de',              // University of Jena resources
-            'https://s3.uni-jena.de',               // S3 storage for user-uploaded content
-            'https://orcid.org',                    // ORCID logos
-            'https://ui-avatars.com',               // UI Avatars service for profile pictures
-            'https://www.nfdi4chem.de',             // NFDI4Chem logos and assets
-            'https://www.nmrium.org',               // NMRium branding assets
-            'https://upload.wikimedia.org',         // Wikipedia/Wikimedia images (ChEBI logo, etc.)
-            'https://pbs.twimg.com'                 // Twitter profile images
-        );
+        // Add image sources using chained add() calls for better compatibility
+        $policy
+            ->add(Directive::IMG, Keyword::SELF)
+            ->add(Directive::IMG, 'data:')
+            ->add(Directive::IMG, 'blob:')
+            ->add(Directive::IMG, 'https://www.uni-jena.de')              // University of Jena resources
+            ->add(Directive::IMG, 'https://s3.uni-jena.de')               // S3 storage for user-uploaded content
+            ->add(Directive::IMG, 'https://orcid.org')                    // ORCID logos
+            ->add(Directive::IMG, 'https://ui-avatars.com')               // UI Avatars service for profile pictures
+            ->add(Directive::IMG, 'https://www.nfdi4chem.de')             // NFDI4Chem logos and assets
+            ->add(Directive::IMG, 'https://www.nmrium.org')               // NMRium branding assets
+            ->add(Directive::IMG, 'https://upload.wikimedia.org')         // Wikipedia/Wikimedia images (ChEBI logo, etc.)
+            ->add(Directive::IMG, 'https://pbs.twimg.com');                // Twitter profile images
 
         // Connection sources - External APIs for data retrieval and services
-        $policy->add(Directive::CONNECT,
+        // Using chained add() calls for better compatibility
+        $policy
             // DOI and metadata APIs (configurable via env)
-            env('DATACITE_API', 'https://api.datacite.org'),
-            env('CROSSREF_API', 'https://api.crossref.org/works/'),
-            env('DATACITE_ENDPOINT', 'https://api.datacite.org'),
-
+            ->add(Directive::CONNECT, env('DATACITE_API', 'https://api.datacite.org'))
+            ->add(Directive::CONNECT, env('CROSSREF_API', 'https://api.crossref.org/works/'))
+            ->add(Directive::CONNECT, env('DATACITE_ENDPOINT', 'https://api.datacite.org'))
+            
             // Chemical data and research APIs (using env config where possible)
-            env('NMRKIT_URL', 'https://nodejs.nmrxiv.org'),
-            config('services.pubchem.base_url'),
-            config('services.cas.base_url'),
-            config('services.chemistry_standardize.url'),
-            env('EUROPEMC_WS_API', 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'),
-            env('ORCID_ID_SEARCH_API', 'https://pub.orcid.org/v2.1/search'),
-            config('services.chemotion_tracker.base_url')
-        );
+            ->add(Directive::CONNECT, env('NMRKIT_URL', 'https://nodejs.nmrxiv.org'))
+            ->add(Directive::CONNECT, config('services.pubchem.base_url'))
+            ->add(Directive::CONNECT, config('services.cas.base_url'))
+            ->add(Directive::CONNECT, config('services.chemistry_standardize.url'))
+            ->add(Directive::CONNECT, env('EUROPEMC_WS_API', 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'))
+            ->add(Directive::CONNECT, env('ORCID_ID_SEARCH_API', 'https://pub.orcid.org/v2.1/search'))
+            ->add(Directive::CONNECT, config('services.chemotion_tracker.base_url'));
 
         // Font sources - External font services
-        $policy->add(Directive::FONT,
-            'https://fonts.googleapis.com',
-            'https://fonts.gstatic.com'
-        );
+        $policy
+            ->add(Directive::FONT, 'https://fonts.googleapis.com')
+            ->add(Directive::FONT, 'https://fonts.gstatic.com');
     }
 }
