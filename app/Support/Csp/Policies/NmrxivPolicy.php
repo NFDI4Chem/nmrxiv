f36df7716ec999fc893c920c644030bde273783b
Nisha Sharma <nisha.sharma@uni-jena.de>
2025-11-24 13:35:32 +0100

    feat: csp fixes and add missing endpoints in the policy (#1284)
    * fix: modify nmrxiv policy to fix csp issues
    
    * fix:remove single policy addition and make multiple additions for font-src directive
    
    * pint formatting
    
    * updated policy to include missing directives
    
    * update ORCID API endpoint in csp policy
    
    * more formatting and removal of duplicates.
    
    * NMRIUM_URL
    
    * add nmrium in frame-src and connect-src directives
    
    * revert dev build tag change
    
    * revert missing tags for dev-latest images
    
    * deploy latest csp changes
    
    * fix:revert dev-build changes

--- app/Support/Csp/Policies/NmrxivPolicy.php
+++ app/Support/Csp/Policies/NmrxivPolicy.php
@@ -18,15 +18,14 @@ class NmrxivPolicy implements Preset
             ->add(Directive::FORM_ACTION, Keyword::SELF)
             ->add(Directive::OBJECT, Keyword::NONE);
 
-        // Asset sources
+        // Basic asset sources
         $policy
             ->add(Directive::SCRIPT, Keyword::SELF)
             ->add(Directive::STYLE, Keyword::SELF)
-            ->add(Directive::IMG, Keyword::SELF, 'data:', 'blob:')
-            ->add(Directive::FONT, 'data:', 'https://fonts.bunny.net')
+            ->add(Directive::FONT, 'data:')
             ->add(Directive::CONNECT, Keyword::SELF);
 
-        // Third-party services (nmrXiv specific)
+        // Third-party services
         $policy
             ->add(Directive::STYLE, 'https://fonts.bunny.net')
             ->add(Directive::SCRIPT, 'https://matomo.nfdi4chem.de')
@@ -39,68 +38,77 @@ class NmrxivPolicy implements Preset
         $this->addUnifiedRules($policy);
     }
 
-    /**
-     * Add unified CSP rules that apply to all environments
-     */
     private function addUnifiedRules(Policy $policy): void
     {
-        // Allow inline scripts and styles (needed for the application to function)
+        // Allow inline scripts and styles
         $policy
             ->add(Directive::SCRIPT, Keyword::UNSAFE_INLINE, Keyword::UNSAFE_EVAL)
             ->add(Directive::STYLE, Keyword::UNSAFE_INLINE);
 
-        // Development server support (for local development with Vite and Browser-sync)
+        // Development server support
         $policy
             ->add(Directive::SCRIPT, 'http://localhost:5173', 'http://localhost:3000')
             ->add(Directive::CONNECT, 'ws://localhost:5173', 'http://localhost:5173', 'http://localhost:3000');
     }
 
     /**
-     * Add nmrXiv-specific external sources that the application needs
-     *
-     * This method includes core external sources that are essential for the application.
-     * For runtime-configurable sources without code deployment, use the global
-     * directives in config/csp.php with environment variables:
-     * - CSP_ADDITIONAL_CONNECT_SRC
-     * - CSP_ADDITIONAL_IMG_SRC
-     * - CSP_ADDITIONAL_SCRIPT_SRC
-     * - CSP_ADDITIONAL_STYLE_SRC
+     * Add nmrXiv-specific external sources.
+     * For runtime-configurable sources, use config/csp.php with CSP_ADDITIONAL_* env variables.
      */
     private function addNmrxivSources(Policy $policy): void
     {
-        // Image sources - External domains for logos, avatars, and institutional content
-        $policy->add(Directive::IMG,
-            'https://www.uni-jena.de',              // University of Jena resources
-            'https://s3.uni-jena.de',               // S3 storage for user-uploaded content
-            'https://orcid.org',                    // ORCID logos
-            'https://ui-avatars.com',               // UI Avatars service for profile pictures
-            'https://www.nfdi4chem.de',             // NFDI4Chem logos and assets
-            'https://www.nmrium.org',               // NMRium branding assets
-            'https://upload.wikimedia.org',         // Wikipedia/Wikimedia images (ChEBI logo, etc.)
-            'https://pbs.twimg.com'                 // Twitter profile images
-        );
+        // Image sources
+        $policy
+            ->add(Directive::IMG, Keyword::SELF)
+            ->add(Directive::IMG, 'data:')
+            ->add(Directive::IMG, 'blob:')
+            ->add(Directive::IMG, 'https://www.uni-jena.de')
+            ->add(Directive::IMG, 'https://s3.uni-jena.de')
+            ->add(Directive::IMG, 'https://orcid.org')
+            ->add(Directive::IMG, 'https://ui-avatars.com')
+            ->add(Directive::IMG, 'https://www.nfdi4chem.de')
+            ->add(Directive::IMG, 'https://www.nmrium.org')
+            ->add(Directive::IMG, 'https://nmriumdev.nmrxiv.org')
+            ->add(Directive::IMG, 'https://upload.wikimedia.org')
+            ->add(Directive::IMG, 'https://pbs.twimg.com')
+            ->add(Directive::IMG, 'https://api.cheminf.studio')
+            ->add(Directive::IMG, 'https://api.naturalproducts.net')
+            ->add(Directive::IMG, 'https://dev.api.naturalproducts.net')
+            ->add(Directive::IMG, 'https://placehold.co');
 
-        // Connection sources - External APIs for data retrieval and services
-        $policy->add(Directive::CONNECT,
-            // DOI and metadata APIs (configurable via env)
-            env('DATACITE_API', 'https://api.datacite.org'),
-            env('CROSSREF_API', 'https://api.crossref.org/works/'),
-            env('DATACITE_ENDPOINT', 'https://api.datacite.org'),
+        // Connection sources
+        $policy
+            ->add(Directive::CONNECT, env('DATACITE_API', 'https://api.datacite.org'))
+            ->add(Directive::CONNECT, env('CROSSREF_API', 'https://api.crossref.org/works/'))
+            ->add(Directive::CONNECT, env('DATACITE_ENDPOINT', 'https://api.datacite.org'))
+            ->add(Directive::CONNECT, env('NMRKIT_URL', 'https://nodejs.nmrxiv.org'))
+            ->add(Directive::CONNECT, config('services.pubchem.base_url'))
+            ->add(Directive::CONNECT, config('services.cas.base_url'))
+            ->add(Directive::CONNECT, config('services.chemistry_standardize.url'))
+            ->add(Directive::CONNECT, env('EUROPEMC_WS_API', 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'))
+            ->add(Directive::CONNECT, env('ORCID_ID_SEARCH_API', 'https://pub.orcid.org'))
+            ->add(Directive::CONNECT, config('services.chemotion_tracker.base_url'))
+            ->add(Directive::CONNECT, env('CM_API', 'https://api.cheminf.studio'))
+            ->add(Directive::CONNECT, env('AWS_ENDPOINT', 'https://s3.uni-jena.de'))
+            ->add(Directive::CONNECT, 'https://nmrium.nmrxiv.org')
+            ->add(Directive::CONNECT, 'https://nmriumdev.nmrxiv.org')
+            ->add(Directive::CONNECT, 'https://api.cheminf.studio')
+            ->add(Directive::CONNECT, 'https://api.naturalproducts.net')
+            ->add(Directive::CONNECT, 'https://dev.api.naturalproducts.net');
 
-            // Chemical data and research APIs (using env config where possible)
-            env('NMRKIT_URL', 'https://nodejs.nmrxiv.org'),
-            config('services.pubchem.base_url'),
-            config('services.cas.base_url'),
-            config('services.chemistry_standardize.url'),
-            env('EUROPEMC_WS_API', 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'),
-            env('ORCID_ID_SEARCH_API', 'https://pub.orcid.org/v2.1/search'),
-            config('services.chemotion_tracker.base_url')
-        );
+        // Font sources
+        $policy
+            ->add(Directive::FONT, 'https://fonts.googleapis.com')
+            ->add(Directive::FONT, 'https://fonts.gstatic.com')
+            ->add(Directive::FONT, 'https://fonts.bunny.net');
 
-        // Font sources - External font services
-        $policy->add(Directive::FONT,
-            'https://fonts.googleapis.com',
-            'https://fonts.gstatic.com'
-        );
+        // Frame sources
+        $policy
+            ->add(Directive::FRAME, Keyword::SELF)
+            ->add(Directive::FRAME, 'https://api.cheminf.studio')
+            ->add(Directive::FRAME, 'https://api.naturalproducts.net')
+            ->add(Directive::FRAME, 'https://dev.api.naturalproducts.net')
+            ->add(Directive::FRAME, 'https://nmrium.nmrxiv.org')
+            ->add(Directive::FRAME, 'https://nmriumdev.nmrxiv.org');
     }
 }
