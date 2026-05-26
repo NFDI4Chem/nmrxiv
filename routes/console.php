7b0dd911443cc6c76db9aa4b84af12736c70dff1
Venkata Nainala <mailcs76@gmail.com>
2026-05-26 18:18:01 +0100

    fix(schedule): re-enable daily publishing of released projects
    Restore the nmrxiv:publish schedule and disable nmrxiv:delete-projects
    so embargoed projects publish on release date instead of being removed.

--- routes/console.php
+++ routes/console.php
@@ -18,8 +18,8 @@ Artisan::command('nmrxiv', function () {
     $this->comment('Welcome to nmrXiv!');
 })->purpose('Display nmrxiv info');
 
-// Schedule::command('nmrxiv:publish')->daily();
-Schedule::command('nmrxiv:delete-projects')->daily();
+Schedule::command('nmrxiv:publish')->daily();
+// Schedule::command('nmrxiv:delete-projects')->daily();
 Schedule::command('nmrxiv:index-molecules')->daily();
 Schedule::command('nmrxiv:delete-citations')->weekly();
 Schedule::command('nmrxiv:delete-authors')->weekly();
