4ce55dbc085f54dd1fedc0700ae9e6c605544dec
Nisha Sharma <sharmanis14@gmail.com>
2026-06-03 13:01:54 +0200

    chemistry/standardize

--- config/external-links.php
+++ config/external-links.php
@@ -32,7 +32,7 @@ return [
     'nmrium_url' => env('NMRIUM_URL', 'https://nmrium.nmrxiv.org'),
     'nmrkit_url' => env('NMRKIT_URL', 'https://nodejs.nmrxiv.org'),
     'europemc_ws_api' => env('EUROPEMC_WS_API', 'https://www.ebi.ac.uk/europepmc/webservices/rest/search'),
-    'cm_api' => env('CM_API', 'https://api.cheminf.studio'),
+    'cm_api' => env('CM_API', 'https://api2.naturalproducts.net/latest/'),
     'crossref_api' => env('CROSSREF_API', 'https://api.crossref.org/works/'),
     'datacite_api' => env('DATACITE_API', 'https://api.datacite.org'),
 
