f270bdbc6cba38b359ee5549cc62df5c45be1d48
Venkata Nainala <mailcs76@gmail.com>
2026-06-03 23:45:24 +0100

    fix(tests): use RefreshDatabase for public Inertia page tests

--- tests/Feature/PublicProjectsPageTest.php
+++ tests/Feature/PublicProjectsPageTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class PublicProjectsPageTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_public_projects_page_can_be_rendered(): void
     {
         $this->get('/projects')
--- tests/Feature/WelcomePageTest.php
+++ tests/Feature/WelcomePageTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Inertia\Testing\AssertableInertia as Assert;
 use Tests\TestCase;
 
 class WelcomePageTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_welcome_page_can_be_rendered(): void
     {
         $this->get('/')
